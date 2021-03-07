905424a670f3becf97d35ea32e9bc6afc2e8430f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-03-07 19:59:15 +0800

    test?

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,33 @@ import ColorSwitcher from '../components/color-switcher'
 import { ThemeProvider, merge } from 'theme-ui'
 import theme from 'theme-ui-preset-geist'
 
+theme.util = {
+  motion: '@media (prefers-reduced-motion: no-preference)',
+  reduceMotion: '@media (prefers-reduced-motion: reduce)',
+  reduceTransparency: '@media (prefers-reduced-transparency: reduce)',
+  supportsClipText: '@supports (-webkit-background-clip: text)',
+  supportsBackdrop:
+    '@supports (-webkit-backdrop-filter: none) or (backdrop-filter: none)',
+  cx: null,
+  gx: null,
+  gxText: null,
+}
+theme.util.cx = c => theme.colors[c] || c
+theme.util.gx = (from, to) => `radial-gradient(
+  ellipse farthest-corner at top left,
+  ${theme.util.cx(from)},
+  ${theme.util.cx(to)}
+)`
+theme.util.gxText = (from, to) => ({
+  color: theme.util.cx(to),
+  [theme.util.supportsClipText]: {
+    backgroundImage: theme.util.gx(from, to),
+    backgroundRepeat: 'no-repeat',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+  },
+})
+
 const brandTheme = merge(theme, {
   initialColorModeName: 'dark',
   useColorSchemeMediaQuery: false,
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,63 @@
-import { Box, Button, Container } from 'theme-ui'
+import { Box, Button, Container, Heading, Text } from 'theme-ui'
 import MapChart from '../components/map'
+import theme from 'theme-ui-preset-geist'
 
 function App() {
   return (
     <Box>
-      <Box sx={{ maxHeight: '100vh', overflowY: 'hidden'}}>
+      <Box sx={{ maxHeight: '100vh', overflowY: 'hidden' }}>
         <MapChart />
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: 20,
+            left: 20,
+            maxHeight: '300px',
+            width: '340px',
+            borderRadius: '8px',
+            padding: '24px',
+            bg: 'gray.1',
+            boxShadow: 'rgba(17, 17, 17, 0.2) 0px 4px 12px;',
+          }}
+        >
+          <Heading as="h1" sx={{ fontWeight: '800', marginBottom: '12px' }}>
+            Share{' '}
+            <Text sx={theme => theme.util.gxText('green', 'gold')}>
+              Australia's
+            </Text>{' '}
+            Pwaa-tastic Spirit with the World
+          </Heading>
+          <Button
+            variant="primary"
+            as="a"
+            href="https://github.com/lachlanjc/next-theme-starter"
+          >
+            Send a Pwaa!!
+          </Button>
+          <Button
+            variant="primary"
+            as="a"
+            sx={{ minWidth: '10px', marginLeft: '8px' }}
+            href="https://github.com/lachlanjc/next-theme-starter"
+          >
+            🔊
+          </Button>
+          <Box sx={{ fontWeight: '400', fontSize: '0.6em', marginTop: '12px' }}>
+            Data points displayed on the map is a combination of Pwaas sent and
+            OnePwaa's website visits.
+          </Box>
+        </Box>
       </Box>
-      This is
-      [@lachlanjc/next-theme-starter](https://github.com/lachlanjc/next-theme-starter)
-      in action.
-      <Button
-        variant="success"
-        as="a"
-        href="https://github.com/lachlanjc/next-theme-starter"
-      >
-        GitHub
-      </Button>
     </Box>
   )
+}
+
+export function getServerSideProps(context) {
+  var ip = context.req.headers['x-forwarded-for'] || context.req.connection.remoteAddress
+  console.log(ip)
+  return {
+    props: { data: 'hi' },
+  }
 }
 
 export default App
