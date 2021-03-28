4d0812047f093a023a79001bf222186cf3c7a9f6
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-03-28 16:14:48 +0800

    remove animate.min.css

--- pages/_app.js
+++ pages/_app.js
@@ -124,10 +124,6 @@ const App = ({ Component, pageProps }) => {
     <ThemeProvider theme={brandTheme}>
       <PusherProvider {...config}>
         <Meta>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
-          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
